5cec01c5396259ed1ed0ba8c1b80eaa1eb2f5c89
Tom Grootjans <tom.grootjans@zigt.nl>
2023-02-02 14:02:57 +0100

    Change format to comply with coding standards

--- src/Theme/ThemeNegotiator.php
+++ src/Theme/ThemeNegotiator.php
@@ -7,35 +7,32 @@ use Drupal\Core\Routing\RouteMatchInterface;
 
 /**
  * Class ThemeNegotiator.
+ *
+ * Override theme for specific routes.
  */
-class ThemeNegotiator implements ThemeNegotiatorInterface
-{
-    /**
-     * {@inheritdoc}
-     */
-    public function applies(RouteMatchInterface $route_match)
-    {
-        // Use this theme on a certain route.
-        // return $route_match->getRouteName() == 'example_route_name';
+class ThemeNegotiator implements ThemeNegotiatorInterface {
 
-        // Or use this for more than one route:
-        $possible_routes = array(
-            'user.login',
-            'user.pass',
-            'user.register',
-            'user.reset',
-            'user.reset.login',
-            'user.reset.form',
-        );
-        return (in_array($route_match->getRouteName(), $possible_routes));
-    }
+  /**
+   * {@inheritdoc}
+   */
+  public function applies(RouteMatchInterface $route_match) {
+    $possible_routes = [
+      'user.login',
+      'user.pass',
+      'user.register',
+      'user.reset',
+      'user.reset.login',
+      'user.reset.form',
+    ];
+    return (in_array($route_match->getRouteName(), $possible_routes));
+  }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function determineActiveTheme(RouteMatchInterface $route_match)
-    {
-        // Here you return the actual theme name.
-        return 'logintheme';
-    }
+  /**
+   * {@inheritdoc}
+   */
+  public function determineActiveTheme(RouteMatchInterface $route_match) {
+    // Here you return the actual theme name.
+    return 'logintheme';
+  }
+
 }
